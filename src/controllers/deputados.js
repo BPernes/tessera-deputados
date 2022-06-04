1cb1dee61c2ce8072c1ac3f7894699e866d4e99f
UtSensum <bernardopvsouza@gmail.com>
2022-06-03 21:19:13 -0300

    fix: correção de bug referente ao cors policy

--- src/controllers/deputados.js
+++ src/controllers/deputados.js
@@ -1,24 +1,28 @@
 const Deputados = require('../domain/models/deputados')
 
 exports.getAll = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
     await Deputados.find({}).then((Deputados) => {
         res.send(Deputados);
     })
 }
 
 exports.getByName = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
     const { nome } = req.params;
     const deputado = await Deputados.find({ nome }).exec()
     res.send(deputado)
 }
 
 exports.getBySiglaUf = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
     const { siglaUf } = req.params;
     const deputados = await Deputados.find({ siglaUf }).exec()
     res.send(deputados)
 }
 
 exports.getBySiglaPartido = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
     const { siglaPartido } = req.params;
     const deputados = await Deputados.find({ siglaPartido }).exec()
     res.send(deputados)
